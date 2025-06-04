3315b361569b9329f13e47820d2660aa8dbf8a61
dudalss <myemail@emap.com>
2025-06-04 14:11:59 +0900

    [feature] 49p

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,6 +1,8 @@
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
@@ -20,7 +22,6 @@ class GameTest {
     private void assertIllegalArgument(String guessNumber) {
         try {
             game.guess(guessNumber);
-            fail();
         } catch (IllegalArgumentException e) {
 
         }
@@ -35,4 +36,14 @@ class GameTest {
         assertIllegalArgument("121");
     }
 
+    @Test
+    void returnSolvedResultIfMatchedNumber() {
+        game.question = "123";
+        GuessResult result = game.guess("123");
+
+        assertThat(result).isNotNull();
+        assertThat(result.solved).isEqualTo(true);
+        assertThat(result.strikes).isEqualTo(3);
+        assertThat(result.balls).isEqualTo(0);
+    }
 }
