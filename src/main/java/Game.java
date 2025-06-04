c35f4e3de57ebdcefe2e417e1e0ec0949079e54d
dudalss <myemail@emap.com>
2025-06-04 13:50:31 +0900

    [feature] 39p

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -3,13 +3,21 @@ public class Game {
         if (guessNumber == null) {
             throw new IllegalArgumentException();
         }
+
         if (guessNumber.length() != 3) {
             throw new IllegalArgumentException();
         }
+
         for (char number : guessNumber.toCharArray()) {
             if (number < '0' || number > '9') {
                 throw new IllegalArgumentException();
             }
+        }
+
+        if (guessNumber.charAt(0) == guessNumber.charAt(1)
+                || guessNumber.charAt(0) == guessNumber.charAt(2)
+                || guessNumber.charAt(1) == guessNumber.charAt(2)) {
+            throw new IllegalArgumentException();
         }
     }
 }
