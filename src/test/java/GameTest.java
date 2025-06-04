091a84f72fc02b2f4ecf19fe2bd6ce98005b6d57
dudalss <myemail@emap.com>
2025-06-04 13:37:40 +0900

    [feature] 28p

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -23,4 +23,11 @@ class GameTest {
             game.guess(null);
         });
     }
+
+    @Test
+    void throwExceptionWhenInputLenngthIsUnmatched() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            game.guess("12");
+        });
+    }
 }
